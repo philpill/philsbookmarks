fea93ae57335b2e3c40392670e8390abd2cbfa79
philpill <github@philpill.net>
2026-05-27 21:37:25 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  adapter: netlify({
+    cacheOnDemandPages: true,
+  }),
+});
